9c50a2177c2fa2f6dcb1cb65586b7736e2bb71f5
d.sivashko <56092378+DimaSivashko@users.noreply.github.com>
2020-09-09 23:07:07 +0300

    2nd Lab Done

---
+++ 2nd_LAB/Task23.c
@@ -0,0 +1,35 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#include <conio.h>
+#include <string.h>
+
+int main()
+{
+	//setlocale(LC_ALL, "Rus");
+	char string[100]; 
+	
+	printf("Please, input string: ");
+	gets(string);
+	printf("String: ");
+	puts(string);
+	int counter = 0;
+	int wordLength = 0;
+	int lastSymbol = 0; // Сохранение индекса начала слова
+	int N = 0;
+	printf("\nPlease, input value: ");
+	scanf_s("%d", &N);
+	
+
+	for (int i = 0; i <= strlen(string); i++) {
+		if ((string[i] == ' ') || (string[i] == ',') || (string[i] == '\0'))
+		{
+			wordLength = i - lastSymbol;
+			lastSymbol = i+1;
+			if ((wordLength == 0) || (wordLength <= N)) { counter--; }
+			counter++;
+		}
+	}
+	
+	printf("Count = %d\n", counter);
+	return 0;
+}
