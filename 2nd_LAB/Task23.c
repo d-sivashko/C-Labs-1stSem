e6257f4c047b74c844290ebfdc58a1708441ca2e
d.sivashko <56092378+DimaSivashko@users.noreply.github.com>
2020-09-10 16:49:12 +0300

    Fix for scheme

--- 2nd_LAB/Task23.c
+++ 2nd_LAB/Task23.c
@@ -7,14 +7,14 @@ int main()
 {
 	//setlocale(LC_ALL, "Rus");
 	char string[100]; 
-	
-	printf("Please, input string: ");
-	gets(string);
 	int counter = 0;
 	int wordLength = 0;
 	int lastSymbol = 0; // Сохранение индекса начала слова
 	int N = 0;
-	printf("\nPlease, input value: ");
+	
+	printf("Please, input string: ");
+	gets(string);
+	printf("Please, input value: ");
 	scanf_s("%d", &N);
 	
 
@@ -28,6 +28,6 @@ int main()
 		}
 	}
 	
-	printf("Count = %d\n", counter);
+	printf("Count = %d", counter);
 	return 0;
 }
