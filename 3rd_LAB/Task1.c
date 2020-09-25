6c8c308d40eb624f1f08447e59d27ba612e4f8ee
d.sivashko <56092378+DimaSivashko@users.noreply.github.com>
2020-09-25 10:19:30 +0300

    Add var 1

---
+++ 3rd_LAB/Task1.c
@@ -0,0 +1,40 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	int n = 0;
+	int countplus = 0; 
+	int countminus = 0;
+	int countzero = 0;
+	int summplus = 0; 
+	int summminus = 0;
+	printf("Please, input count: ");
+	scanf_s("%d", &n);
+	for (int i = 1; i <= n; i++)
+	{
+			int c = 0;
+			printf("Please, input number: ");
+			scanf_s("%d", &c);
+			if (c > 0)
+			{
+				countplus++;
+				summplus = summplus + c;
+			}
+			else if (c < 0)
+			{
+				countminus++;
+				summminus = summminus + c;
+			}
+			else
+			{
+				countzero++;
+			}
+
+	}
+	printf("Summplus = %d\n", summplus);
+	printf("Summminus = %d\n", summminus);
+	printf("Value plus = %d\n", countplus);
+	printf("Value minus = %d\n", countminus);
+	printf("Value zero = %d\n", countzero);
+	return 0;
+}
