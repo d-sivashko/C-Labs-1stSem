c9aff4dc0db66f98371e39522fb98186e64faa6b
d.sivashko <56092378+DimaSivashko@users.noreply.github.com>
2020-12-02 13:28:19 +0300

    Done

---
+++ 9th_LAB/main.C
@@ -0,0 +1,37 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+int checkFunc(int num)
+{
+    if (num % 2 == 0) {
+        printf("\nYour number is EVEN");
+    }
+    else {
+        printf("\nYour number is ODD");
+        num++;
+    }
+    return num;
+}
+
+float percentFunc(int num)
+{
+    float percentFromNum = 0, percent = 0;
+    printf("\nInput b: ");
+    scanf_s("%f", &percent);
+    percentFromNum = num * (percent / 100);
+    int a = (int)percentFromNum;
+    if ((percentFromNum  - a)!= 0)
+        printf("\nPercent from num = %f", percentFromNum);
+    else
+        printf("\nPercent from num = %d", a);
+}
+
+int main()
+{
+    float n = 0, b = 0;
+    printf("Input n: ");
+    scanf_s("%f", &n);
+    n = checkFunc(n);
+    percentFunc(n);;
+    return 1;
+}
