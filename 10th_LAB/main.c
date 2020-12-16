59319ad88262e6f8ab8c55c39a507a02947d34b6
d.sivashko <56092378+DimaSivashko@users.noreply.github.com>
2020-12-16 14:38:58 +0300

    10-ая лабораторная, вариант 4. Готово.

---
+++ 10th_LAB/main.c
@@ -0,0 +1,19 @@
+﻿// Ввести с клавиатуры число n. С помощью рекурсивной функции найти факториал числа n. Вывести полученное значение на экран.
+
+#include <stdio.h>
+
+int factorial(int n) {
+	int result;
+	if (n == 1 || n == 0) return(1);
+	result = factorial(n - 1) * n;
+	return (result);
+}
+
+int main(){
+	int n = 0, result = 0;
+	printf("Please, input number: ");
+	scanf("%d", &n);
+	result = factorial(n);
+	printf("Result = %d", result);
+	return 0;
+}
